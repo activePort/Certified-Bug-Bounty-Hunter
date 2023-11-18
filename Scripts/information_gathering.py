fadbd334ae2024fa8e3864897b8492788491a2f4
activePort <nicolasgula2023@gmail.com>
2023-11-17 20:49:32 -0500

    add cheat sheet ffuf

--- Scripts/information_gathering.py
+++ Scripts/information_gathering.py
@@ -1,4 +1,5 @@
 import whois
+import subprocess
 from colorama import Fore, Style, Back 
 
 # WHOIS
@@ -9,8 +10,18 @@ def consulte_whois(domain_name):
     except whois.parser.PywhoisError as e:
         print(f"Error al consultar WHOIS: {e}")
 
-
+# DNS
+def dig_all_records(domain_name):
+    try:
+        result = subprocess.check_output(["dig", domain_name, "ANY"], universal_newlines=True)
+        print(result)
+    except subprocess.CalledProcessError as e:
+        print(f"Error al ejecutar dig: {e}")
 
 print()
 domain_name = input(Fore.RED + Back.GREEN + "Enter the domain name to query:" + Style.RESET_ALL + " ")
+print()
+print(Fore.RED + Back.GREEN + "### WHOIS ###" + Style.RESET_ALL)
 consulte_whois(domain_name)
+print(Fore.RED + Back.GREEN + "### DNS ###" + Style.RESET_ALL)
+dig_all_records(domain_name)
